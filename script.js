87997948e99775e268f1c5eef8315b35394c9be2
ktrebor <robert.kedves.90@gmail.com>
2022-01-29 12:05:57 +0200

    v1.0 script

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const clearButton = document.querySelector('[data-clear]');
 const previousOperand = document.querySelector('[data-previous-operand]');
 const currentOperand = document.querySelector('[data-current-operand]');
+const pointButton = document.querySelector('[data-decimal]');
+
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
 
 function add(a, b) {
     return a + b;
@@ -30,9 +36,9 @@ function operate(operator, a, b) {
         return add(a, b)
       case '-':
         return substract(a, b)
-      case '*':
+      case 'x':
         return multiply(a, b)
-      case '/':
+      case '÷':
         if (b === 0) return null
         else return divide(a, b)
       default:
@@ -40,3 +46,81 @@ function operate(operator, a, b) {
   }
 }
 
+//event listener for digit buttons and append everytime a user press a digit
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+ 
+function appendNumber(number) {
+  if (currentOperand.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  currentOperand.textContent += number  
+}
+
+function resetScreen() {
+  currentOperand.textContent = ''
+  shouldResetScreen = false
+}
+
+//event listener to all clear and  add 0 to current-operand and empty string to previous-operand
+allClearButton.addEventListener('click', allclear);
+function allclear() {
+  currentOperand.textContent = '0'
+  previousOperand.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+//event listener to clear => removes the last element from the new string
+clearButton.addEventListener('click', clear);
+function clear() {
+  currentOperand.textContent = currentOperand.textContent
+  .toString()
+  .slice(0, -1)
+}
+
+
+//event listener to pointbutton => adds a single point to the string
+pointButton.addEventListener('click', appendPoint);
+function appendPoint() {
+  if (shouldResetScreen) resetScreen()
+  if (currentOperand.textContent === '')
+    currentOperand.textContent = '0'
+  if (currentOperand.textContent.includes('.')) return
+  currentOperand.textContent += '.'
+}
+
+//event listener to all opperands
+operationButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentOperand.textContent
+  currentOperation = operator
+  previousOperand.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+//event listener to equals
+equalsButton.addEventListener('click', evaluate);
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentOperand.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = currentOperand.textContent
+  currentOperand.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  )
+  previousOperand.textContent = `${firstOperand} ${currentOperation} ${secondOperand}`
+  currentOperation = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
